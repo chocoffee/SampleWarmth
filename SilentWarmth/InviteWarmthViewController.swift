6daea7c1d3e9689c76f9e46b3476af68520a4304
chocoffee <chocoffee21@gmail.com>
2016-11-28 16:38:58 +0900

    test

--- SilentWarmth/InviteWarmthViewController.swift
+++ SilentWarmth/InviteWarmthViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class InviteWarmthViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class InviteWarmthViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, CBPeripheralDelegate, CBPeripheralManagerDelegate {
     @IBOutlet weak var attributePicker: UIPickerView!
     @IBOutlet weak var colorPicker: UIPickerView!
     @IBOutlet weak var isEdge: UISwitch!
     
-    let attribute = ["お年寄り", "妊婦さん", "子連れさん", "けが人", "体調不良"]
-    let color = ["赤", "青", "黄", "白", "黒"]
+//    let attribute = ["お年寄り", "妊婦さん", "子連れさん", "けが人", "体調不良"]
+//    let color = ["赤", "青", "黄", "白", "黒"]
+    var advertiseData = [[]]
+    let alert = AleatBase()
+    
+    var peripheral: CBPeripheral!
+    var peripheralManager: CBPeripheralManager!
+    var characteristic: CBMutableCharacteristic!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,37 +33,105 @@ class InviteWarmthViewController: UIViewController, UIPickerViewDelegate, UIPick
         colorPicker.delegate = self
         colorPicker.dataSource = self
         
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        
         self.title = "ゆずってほしい"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    //  pickerの横列のアレ
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
+    //  pickerの縦列のアレ
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        print("tag = \(pickerView.tag)")
         if pickerView.tag == 1 {
-            return self.attribute.count
+            return Values.attribute.count
         }else{
-            return self.color.count
+            return Values.color.count
         }
     }
     
+    //  pickerに値をセット
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if pickerView.tag == 1 {
-            return self.attribute[row]
+            return Values.attribute[row]
         }else {
-            return self.color[row]
+            return Values.color[row]
         }
     }
     
-    
-
+    //  あらーと挟んでアドバタイズ
     @IBAction func inviteWarmth(_ sender: UIButton) {
+        var str = ""
+        self.advertiseData = [
+            [   1
+            ],[
+                attributePicker.selectedRow(inComponent: 0)
+            ],[
+                colorPicker.selectedRow(inComponent: 0)
+            ],[
+                isEdge.isOn ? 1 : 0
+            ]
+        ]
+        str.append("\(Values.attribute[attributePicker.selectedRow(inComponent: 0)]), ")
+        str.append("服：\(Values.color[colorPicker.selectedRow(inComponent: 0)]), ")
+        if isEdge.isOn {
+            str.append("端")
+        }
+        
+        setAlert(str)
+    }
+    
+    //  あらーと呼び出し
+    func setAlert(_ str: String) {
+        alert.alert("発信する", btn2: "キャンセル", title: "確認", subTitle: str, advertise: advertise)
+    }
+    
+    
+    //  以下CB用メソッド
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        print("peripheralState: \(peripheral.state)")
+    }
+    
+    //  アドバタイズするデータ準備
+    func advertise() {
+        guard let json = try? JSONSerialization.data(withJSONObject: advertiseData, options: .init(rawValue: 0)) else{
+            return
+        }
+        let serviceUUID = CBUUID(string: Uuids.serviceUUID)
+        let service = CBMutableService(type: serviceUUID, primary: true)
+        let charactericUUID = CBUUID(string: Uuids.characteristicUUID)
+        characteristic = CBMutableCharacteristic(type: charactericUUID, properties: CBCharacteristicProperties.read, value: json, permissions: CBAttributePermissions.readable)
+        service.characteristics = [characteristic]
+        self.peripheralManager.add(service)
+        
+        let advertisementData = [CBAdvertisementDataServiceUUIDsKey:[service.uuid]] as [String : Any]
+        
+        peripheralManager.startAdvertising(advertisementData)
+        
+    }
+    
+    //  service add result
+    func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
+        if error != nil {
+            print("service add failed...")
+            return
+        }
+        print("service add success!")
+    }
+    
+    //  advertise result
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        if error != nil {
+            print("advertising error...")
+            return
+        }
+        print("advertising success!")
     }
 }
